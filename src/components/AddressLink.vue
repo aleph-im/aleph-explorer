9903bdf1e107bfabc5d75cf3bfaebbeee1c78732
moshe malawach <moshe.malawach@protonmail.com>
2019-06-21 12:41:51 +0200

    new addresslink component

---
+++ src/components/AddressLink.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-link :class="'address'+this.class"
+          :to="{name: 'address-detail', params: {
+               address: this.address, chain: this.chain
+               }}">
+    {{this.address}}</b-link>
+</template>
+
+<script>
+export default {
+  name: 'address-link',
+  props: {
+    address: String,
+    chain: String,
+    class: String
+  }
+}
+</script>
