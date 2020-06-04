776fb46013f8c169ca44ecfcdaeb086564902bc2
Moshe Malawach <moshe.malawach@protonmail.com>
2020-06-05 00:40:35 +0200

    version and info

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@
           <ul class="navbar-nav mr-auto mr-3">
             <li v-if="window.width < 1024"><b-link @click="display_menu = !display_menu" href="#" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></b-link></li>
           </ul>
-          <b-navbar-brand v-if="window.width < 1024" to="/">א test-net</b-navbar-brand>
-          <b-navbar-brand v-else to="/">א test-net Explorer</b-navbar-brand>
+          <b-navbar-brand v-if="window.width < 1024" to="/">א.im</b-navbar-brand>
+          <b-navbar-brand v-else to="/">א.im network Explorer</b-navbar-brand>
           <!-- <form class="form-inline ml-auto">
             <ul class="navbar-nav">
               <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
@@ -28,10 +28,10 @@
         </div>
         <footer class="main-footer px-5">
           <div class="footer-left">
-            Copyright © 2019 <a href="https://aleph.im">Aleph.im</a>
+            Copyright © 2018-2020 <a href="https://aleph.im">Aleph.im</a>
           </div>
           <div class="footer-right">
-            v0.1.0
+            v0.1.1
             <a href="https://github.com/aleph-im/aleph-explorer" class="card-link"
                  target="_blank" rel="noopener noreferrer">
                 <i class="fab fa-github"></i>
