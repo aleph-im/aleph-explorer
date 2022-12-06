2fcfbc2295cd8359ab3b2c922481cc7c535556ed
Bonjour Internet <BjrInt@users.noreply.github.com>
2022-12-06 19:03:49 +0100

    Feat: Channel filtering as a text input
    Enhances granularity over search results and gives a better user experience

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -1,42 +1,38 @@
 <template>
   <div>
-    <b-dropdown id="dropdown-1" variant="outline-light"
-                :text="selchannel?selchannel:'All channels'" class="position-absolute mt-n5 rounded ml-4 ml-lg-0">
-      <b-dropdown-item @click="select_channel(null)">All channels</b-dropdown-item>
-      <b-dropdown-divider></b-dropdown-divider>
-      <b-dropdown-item v-for="channel of channels" @click="select_channel(channel)" :key="channel">
-        {{channel}}
-      </b-dropdown-item>
-    </b-dropdown>
-      <b-card no-body class="card-primary">
-        <b-card-header class="d-flex justify-content-between">
-          <h4>Messages</h4>
+    <b-form-input list="channel-datalist" 
+                  @input="select_channel"
+                  :state="channelExists()" />
+    <datalist id="channel-datalist">
+      <option v-for="channel of channels" :key="channel">{{ channel }}</option>
+    </datalist>
 
-          <b-pagination
-            v-model="page"
-            :total-rows="total_msg"
-            :per-page="per_page"
-            limit="4"
-            class="mb-0" size="sm"
-          ></b-pagination>
-        </b-card-header>
-        <MessageList :messages="messages" class="compact" detailed />
+    <b-card no-body class="card-primary">
+      <b-card-header class="d-flex justify-content-between">
+        <h4>Messages</h4>
 
-        <b-card-footer class="d-flex justify-content-between bg-whitesmoke">
-          Total: {{total_msg}}
-          <b-pagination
-            v-model="page"
-            :total-rows="total_msg"
-            :per-page="per_page"
-            limit="9"
-            class="mb-0" size="sm"
-          ></b-pagination>
-        </b-card-footer>
-        <!--
-        <b-card-body class="p-0">
-          <MessageTable :messages="last_messages" striped hover table-class="compact mb-0 table-nowrap" />
-        </b-card-body> -->
-      </b-card>
+        <b-pagination
+          v-model="page"
+          :total-rows="total_msg"
+          :per-page="per_page"
+          limit="4"
+          class="mb-0" size="sm"
+        ></b-pagination>
+      </b-card-header>
+
+      <MessageList :messages="messages" class="compact" detailed />
+
+      <b-card-footer class="d-flex justify-content-between bg-whitesmoke">
+        Total: {{total_msg}}
+        <b-pagination
+          v-model="page"
+          :total-rows="total_msg"
+          :per-page="per_page"
+          limit="9"
+          class="mb-0" size="sm"
+        ></b-pagination>
+      </b-card-footer>
+    </b-card>
   </div>
 </template>
 
@@ -45,10 +41,11 @@ import { mapState } from 'vuex'
 import MessageList from '@/components/MessageList.vue'
 import axios from 'axios'
 import VueJsonPretty from 'vue-json-pretty'
+import debounce from 'lodash/debounce'
 
 export default {
   name: 'about',
-  data() {
+  data () {
     return {
       messages: [],
       channels: [],
@@ -72,11 +69,11 @@ export default {
     MessageList, VueJsonPretty
   },
   methods: {
-    async refresh() {
+    async refresh () {
       await this.getMessages()
       await this.getChannels()
     },
-    async getMessages() {
+    async getMessages () {
       let response = await axios.get(`${this.api_server}/api/v0/messages.json`, {
         params: {
           'pagination': this.per_page,
@@ -89,26 +86,31 @@ export default {
       this.messages = messages // display all for now
       this.total_msg = response.data.pagination_total
     },
-    async getChannels() {
+    async getChannels () {
       let response = await axios.get(`${this.api_server}/api/v0/channels/list.json`)
       let channels = response.data.channels
 
       this.channels = channels // display all for now
     },
-    select_channel(channel) {
+    select_channel (channel) {
       this.selchannel = channel
-      this.getMessages()
+
+      if(!this.selchannel || this.channelExists())
+        debounce(this.getMessages(), 300)
+    },
+    channelExists () {
+      return this.channels.find(f => this.selchannel === f) !== undefined
     }
   },
   watch: {
-    async $route(to, from) {
+    async $route (to, from) {
       await this.refresh()
     },
-    async page() {
+    async page () {
       await this.refresh()
     }
   },
-  async created() {
+  async created () {
     await this.refresh()
   }
 }
