c3b20938be073323c0811443287dccb376f80402
Bonjour Internet <BjrInt@users.noreply.github.com>
2022-12-08 16:32:48 +0100

    feat: Display last git tag on UI

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
+const { execSync } = require('child_process')
+const webpack = require('webpack')
+
+const getGitDescription = () => {
+  try {
+    return execSync('git describe --tags').toString()
+  } catch (error) {
+    return null
+  }
+}
+
 module.exports = {
   pluginOptions: {
     moment: {
@@ -5,5 +16,12 @@ module.exports = {
         'en'
       ]
     }
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        GIT_DESCRIBE_TAGS: JSON.stringify(getGitDescription())
+      })
+    ]
   }
 }
