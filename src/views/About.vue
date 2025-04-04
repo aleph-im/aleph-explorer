e91fd3b2295edeca93e5519ea1af305c54e16f7e
gmolki <gerardmo98@gmail.com>
2025-04-04 14:08:12 +0200

    refactor: use Aleph Cloud for Aleph.im copies

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,14 +2,15 @@
   <div>
     <b-card no-body>
       <b-card-header>
-        <h4>What is
-          <a href="https://aleph.im"
-           target="_blank" rel="noopener noreferrer">Aleph.im</a>?</h4>
+        <h4>
+          What is
+          <a href="https://aleph.im" target="_blank" rel="noopener noreferrer"
+            >Aleph Cloud</a
+          >?
+        </h4>
       </b-card-header>
       <b-card-body>
-        <b-card-text>
-          Aleph.im is:
-        </b-card-text>
+        <b-card-text> Aleph Cloud is: </b-card-text>
         <ul>
           <li>A cross-blockchain layer-2 network</li>
           <li>A decentralized secure cloud computing network</li>
@@ -17,22 +18,30 @@
         </ul>
       </b-card-body>
       <b-card-footer class="bg-whitesmoke">
-        <a href="https://aleph.im" class="card-link"
-           target="_blank" rel="noopener noreferrer">
-           <i class="fas fa-home"></i> Website</a>
-        <a href="https://github.com/aleph-im/" class="card-link"
-           target="_blank" rel="noopener noreferrer">
-          <i class="fab fa-github"></i> Github repositories</a>
+        <a
+          href="https://aleph.im"
+          class="card-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fas fa-home"></i> Website</a
+        >
+        <a
+          href="https://github.com/aleph-im/"
+          class="card-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fab fa-github"></i> Github repositories</a
+        >
       </b-card-footer>
     </b-card>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'about',
-  components: {
-  }
-}
+  name: "about",
+  components: {},
+};
 </script>
